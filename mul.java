576085d4b737750269757fa51eeab73dde630236
Pratham <thakurpratham8408@gmail.com>
2024-03-06 23:35:38 +0530

    Committed Again

---
+++ mul.java
@@ -0,0 +1,9 @@
+public class mul {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 3;
+        int d = 5;
+        System.out.println(a * b * c * d);
+    }
+}
